32422898038c4e6f3dfbab5cce1a5fdf88c3245a
Haji Shikanda <hajjabdullahi007@gmail.com>
2025-09-17 18:00:01 +0300

    First day in the Python Bootcamp

---
+++ challenge_profit.py
@@ -0,0 +1,13 @@
+""""
+A company's revenue is 45,897,513.
+
+Calculate the company's profit if the profit represents 12.7% of the revenue.
+
+Display the profit using 2 decimal places.
+"""
+
+revenue = 45_897_513
+profit = revenue * 12.7/100
+print(f"Revenue: {revenue}")
+print(f"Percentage Profit: 12.7")
+print("Profit: ", format(profit, '.2f') )
---
+++ kiloMeter_calculator.py
@@ -0,0 +1,4 @@
+miles =float(input("Enter the value in Miles: "))
+print(f"Miles = {miles}")
+kilometers = miles * 1.609
+print(f"km = {kilometers}")
